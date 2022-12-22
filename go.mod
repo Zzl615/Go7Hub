5f9f79f3a890f2c84cb8e783d25ceb7306a37c4c
zhangzongliang <zhangzongliang@zuoshouyisheng.com>
2022-12-23 00:19:19 +0800

    Feat: 写小代码 - gorilla.handlers

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Zzl615/Go7Hub
+
+go 1.18
+
+require github.com/gorilla/handlers v1.5.1
+
+require github.com/felixge/httpsnoop v1.0.1 // indirect
---
+++ web/test_gorilla.go
@@ -0,0 +1,34 @@
+/**
+ * @Author: Noaghzil
+ * @Date:   2022-12-22 08:15:22
+ * @Last Modified by:   Noaghzil
+ * @Last Modified time: 2022-12-23 00:15:49
+ */
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+)
+
+func ShowAdminDashboard(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ShowAdminDashboard!")
+}
+
+func ShowIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ShowIndex!")
+}
+
+func main() {
+	r := http.NewServeMux()
+
+	// Only log requests to our admin dashboard to stdout
+	r.Handle("/admin", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(ShowAdminDashboard)))
+	r.HandleFunc("/", ShowIndex)
+
+	// Wrap our server with our gzip handler to gzip compress all responses.
+	http.ListenAndServe(":8000", handlers.CompressHandler(r))
+}
