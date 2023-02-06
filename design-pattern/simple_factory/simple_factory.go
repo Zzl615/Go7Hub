ebb8a7138c9689b29cbba5c2a12ad371209908aa
zhangzongliang <zhangzongliang@zuoshouyisheng.com>
2023-02-06 13:43:42 +0800

    Feat: 新增设计模式

---
+++ design-pattern/simple_factory/simple_factory.go
@@ -0,0 +1,22 @@
+package simplefactory
+
+import "fmt"
+
+// API is interface
+type API interface {
+	Say(name string) string
+}
+
+// HiAPI is one of API implement
+type HiAPI struct {
+	word string = "Hi!"
+}
+
+func (*HiAPI) Say(name string) string {
+	return fmt.Sprintf("%s, %s", HiAPI.word, name)
+}
+
+// NewAPI return API instance by type
+func NewAPI(t int) API {
+	return &HiAPI{}
+}
